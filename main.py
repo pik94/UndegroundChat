5dbca2cd7fff850d0c03af6280cd0bfe7b9b4d6d
pik94 <pinchuk.ilia.work@gmail.com>
2021-03-07 17:40:57 +0300

    Add CLI options
    Use cli to pass arguments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import argparse
 import asyncio
 from asyncio.streams import StreamReader, StreamWriter
 import datetime as dt
 from pathlib import Path
-from typing import Tuple
+from typing import NoReturn, Tuple
 
 import aiofiles
 
@@ -16,22 +17,43 @@ async def connect_to_chat(host: str,
     return reader, writer
 
 
-async def run(host: str, port: int, chat_file: Path):
+async def run(host: str, port: int, history_path: Path) -> NoReturn:
     reader, writer = await connect_to_chat(host, port)
 
     try:
         while True:
             data = await reader.read(READ_SIZE)
             date = dt.datetime.utcnow()
-            async with aiofiles.open(chat_file, 'a') as file:
+            async with aiofiles.open(history_path, 'a') as file:
                 msg = f'[{date.strftime("%Y.%m.%d %H:%M")}] {data.decode()}'
                 await file.write(msg)
     finally:
         writer.close()
 
 
+def main(args: argparse.Namespace) -> NoReturn:
+    host = args.host
+    port = args.port
+    history = Path(args.history)
+    asyncio.run(run(host, port, history))
+
+
 if __name__ == '__main__':
-    host = 'minechat.dvmn.org'
-    port = 5000
-    chat_file = Path('chat.txt')
-    asyncio.run(run(host, port, chat_file))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host',
+                        type=str,
+                        required=False,
+                        default='minechat.dvmn.org',
+                        help='A host of the server which you want to connect')
+    parser.add_argument('--port',
+                        type=int,
+                        required=False,
+                        default=5000,
+                        help='A port of the server which you want to connect')
+    parser.add_argument('--history',
+                        type=str,
+                        required=False,
+                        default='history.txt',
+                        help='A file where you want to save history.')
+    args = parser.parse_args()
+    main(args)
